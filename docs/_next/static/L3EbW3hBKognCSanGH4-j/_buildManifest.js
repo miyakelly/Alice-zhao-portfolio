48c73575a23284ac28793af443be2143eec4306e
miyakelly <liangzhao0801@gmail.com>
2026-04-19 16:28:09 -0700

    upload next/

--- docs/_next/static/L3EbW3hBKognCSanGH4-j/_buildManifest.js
+++ docs/_next/static/L3EbW3hBKognCSanGH4-j/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Alice-zhao-portfolio//_next/:path+",
+        "destination": "/Alice-zhao-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
