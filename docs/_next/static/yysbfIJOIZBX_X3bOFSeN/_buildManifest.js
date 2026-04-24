ee24b3617aec8a7c236ba983e600f2e0a5c28ee3
miyakelly <liangzhao0801@gmail.com>
2026-04-23 23:33:32 -0700

    move CNAME to the docs/ folder and rebuild the app

--- docs/_next/static/yysbfIJOIZBX_X3bOFSeN/_buildManifest.js
+++ docs/_next/static/yysbfIJOIZBX_X3bOFSeN/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Alice-zhao-portfolio//_next/:path+",
-        "destination": "/Alice-zhao-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
